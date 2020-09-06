e57f7159953494aa738b0fa6d6033510f45469a7
Indre <imaksimovaite@gmail.com>
2020-09-06 12:43:36 +0300

    Updates

---
+++ precache-manifest.2f19e605ef99c0f4113511fe9ad6f896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df31af49cd848b774fbd12dc16a0f10",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "2aa8cf2453708435d526",
+    "url": "/todo-list/static/css/main.6ecbc1a8.chunk.css"
+  },
+  {
+    "revision": "978ac7a7250298af1795",
+    "url": "/todo-list/static/js/2.b39017d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.b39017d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa8cf2453708435d526",
+    "url": "/todo-list/static/js/main.8ad00bb0.chunk.js"
+  },
+  {
+    "revision": "a5b6b3bdaf05b08465b6",
+    "url": "/todo-list/static/js/runtime-main.1e8eb3a3.js"
+  }
+]);
